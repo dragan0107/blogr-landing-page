636e5ef583f99ff4047c51121df5c45b42874b3b
drip016 <73792907+drip016@users.noreply.github.com>
2021-12-13 01:27:58 +0100

    finalized the website

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const burgerImg = document.getElementById('burgerImg');
+const burgerSubMenu = document.getElementById('connectOptions');
+
+
+const toggleSubMenu = () => {
+    burgerSubMenu.classList.toggle('showSubMenu');
+}
 const showMenu = () => {
     let iconSrc = document.getElementById('burgerImg').src;
     burgerBox.classList.toggle('menu');
     burgerBox.classList.toggle('transparent');
-    // burgerImg.classList.toggle('closeIcon');
     let srcSplitted = iconSrc.split('/');
     let imgName = srcSplitted[srcSplitted.length-1]
     if (imgName === 'icon-hamburger.svg') {
